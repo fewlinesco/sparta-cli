81c4225529b3ce201e09eda963bf90f18443e547
Vincent Billey <vincent.billey@gmail.com>
2021-04-12 13:07:26 +0200

    Fix sync command for 'empty' days

--- src/services/fetch-past-days-exercises-paths.ts
+++ src/services/fetch-past-days-exercises-paths.ts
@@ -27,7 +27,9 @@ export default async function fetchPastDaysExercisesPaths(
     throw new CalendarFetchError(error.message);
   }
 
-  const paths = findPastDaysExercisesPaths(calendar);
+  const paths = findPastDaysExercisesPaths(calendar).filter(
+    (path) => path !== "",
+  );
 
   return paths;
 }
